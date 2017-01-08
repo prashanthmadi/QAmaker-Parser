ff12c0dd3609d85fc1cd181853e1dc7a3db83327
prashanthmadi <prashanthrmadi@gmail.com>
2017-01-08 00:14:09 -0600

    - storing file in storage and returning sas url - code cleanup

--- app.js
+++ app.js
@@ -1,90 +1,12 @@
 module.exports = function(context, req) {
-
-    var requestCounter = 1;
-    var finalContent = [];
-
+    var util = require('./util.js');
     if (req.body && req.body.gitToken) {
-        gitToken = req.body.gitToken;
-        github_username = req.body.github_username;
-        github_reponame = req.body.github_reponame;
-        branch = req.body.branch;
+        util.gitContent(context, req);
     } else {
-        requestCounter = 0;
-        onCompleted("Please Send Correct Info", null);
-    }
-
-    var GitHub = require('github-api');
-    var marked = require('marked');
-    var tsv = require('tsv');
-    const fs = require('fs');
-    const gh = new GitHub({
-        token: gitToken
-    });
-    let repo = gh.getRepo(github_username, github_reponame);
-
-    // method to crawl github repo
-    var startSpider = function(path) {
-        repo.getContents(branch, path, true, function(error, data) {
-            requestCounter += data.length;
-            requestCounter--;
-            if (error) {
-                context.log(error);
-            }
-            for (var i in data) {
-                if (data[i].type == "dir") {
-                    startSpider(data[i].path);
-                } else {
-                    processFileContent(data[i].path);
-                }
-            }
-        });
-    }
-
-    // process File Content and append content to tsv file
-    var processFileContent = function(filePath) {
-        repo.getContents(branch, filePath, true, function(error, data) {
-            if (error) {
-                context.log(error);
-            }
-            var currentFileData = new Array();
-            var splittedcontent = data.split("---");
-            var res = data.replace(splittedcontent[1], "");
-            data = res.replace("------", "");
-            res = "";
-            var splitTopContent = splittedcontent[1].replace(/^(\r\n)|(\n)/, '').split("\n");
-            for (var i in splitTopContent) {
-                var splitKeyValue = splitTopContent[i].split(":");
-                currentFileData[splitKeyValue[0]] = splitKeyValue[1];
-            }
-            currentFileData.data = marked(data).replace(/\n+/g, ' ');
-            finalContent.push(currentFileData);
-            requestCounter--;
-            onCompleted(null, "Finished Processing successfully");
-        });
-    }
-
-    // final call
-    var onCompleted = function(error, data) {
-        if (requestCounter != 0) {
-            return;
-        }
-        context.bindings.qamaker = tsv.stringify(finalContent);
-        if (error) {
-            res = {
-                status: 500,
-                body: error
-            };
-        } else {
-            res = {
-                status: 200,
-                body: data
-            };
-        }
+        res = {
+            status: 400,
+            body: "Please Send Correct Info"
+        };
         context.done(null, res);
-    }
-
-    // Strating from root url of branch
-    if(requestCounter>0){
-      startSpider("");
     }
 };
---
+++ util.js
@@ -0,0 +1,103 @@
+var GitHub = require('github-api');
+var marked = require('marked');
+var tsv = require('tsv');
+const fs = require('fs');
+
+var requestCounter = 1;
+var finalContent = [];
+const gh = new GitHub({
+    token: gitToken
+});
+let repo = gh.getRepo(github_username, github_reponame);
+
+// method to crawl github repo
+var startSpider = function(path) {
+    repo.getContents(branch, path, true, function(error, data) {
+        requestCounter += data.length;
+        requestCounter--;
+        if (error) {
+            context.log(error);
+        }
+        for (var i in data) {
+            if (data[i].type == "dir") {
+                startSpider(data[i].path);
+            } else {
+                processFileContent(data[i].path);
+            }
+        }
+    });
+}
+
+// process File Content and append content to tsv file
+var processFileContent = function(filePath) {
+    repo.getContents(branch, filePath, true, function(error, data) {
+        if (error) {
+            context.log(error);
+        }
+        var currentFileData = new Array();
+        var splittedcontent = data.split("---");
+        var res = data.replace(splittedcontent[1], "");
+        data = res.replace("------", "");
+        res = "";
+        var splitTopContent = splittedcontent[1].replace(/^(\r\n)|(\n)/, '').split("\n");
+        for (var i in splitTopContent) {
+            var splitKeyValue = splitTopContent[i].split(":");
+            currentFileData[splitKeyValue[0]] = splitKeyValue[1];
+        }
+        currentFileData.data = marked(data).replace(/\n+/g, ' ');
+        finalContent.push(currentFileData);
+        requestCounter--;
+        onCompleted(null, "Finished Processing successfully<br>");
+    });
+}
+
+// final call
+var onCompleted = function(error, data) {
+    if (error) {
+        res = {
+            status: 500,
+            body: error
+        };
+        requestCounter = 0;
+    }
+    if (requestCounter != 0) {
+        return;
+    }
+    context.bindings.qamaker = tsv.stringify(finalContent);
+    var file_name = github_username + "-" + github_reponame;
+    data += "Please find TSV file for " + file_name + " @ " + getSAS("outcontainer", file_name + ".tsv");
+    res = {
+        status: 200,
+        body: data
+    };
+    context.done(null, res);
+}
+
+exports.gitContent = function(cont, req) {
+    context = cont;
+    gitToken = req.body.gitToken;
+    github_username = req.body.github_username;
+    github_reponame = req.body.github_reponame;
+    branch = req.body.branch;
+    startSpider("");
+}
+
+// return SAS Url
+var getSAS = function(containerName, blobName) {
+    var azure = require('azure-storage');
+    var blobService = azure.createBlobService(process.env.AzureWebJobsStorage);
+    var startDate = new Date();
+    var expiryDate = new Date(startDate);
+    expiryDate.setMinutes(startDate.getMinutes() + 60);
+
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: azure.BlobUtilities.SharedAccessPermissions.READ,
+            Start: startDate,
+            Expiry: expiryDate
+        },
+    };
+
+    var token = blobService.generateSharedAccessSignature(containerName, blobName, sharedAccessPolicy);
+    return blobService.getUrl(containerName, blobName, token);
+}
